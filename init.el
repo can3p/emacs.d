e98cf396c5505e492c9de1ec72eb1b14b733e68b
Dmitry Petrov <dpetroff@gmail.com>
2017-08-15 01:03:54 +0200

    untabify file

--- init.el
+++ init.el
@@ -1,13 +1,13 @@
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+             '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
-	(package-refresh-contents)
-	(package-install 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/settings.org"))
 (custom-set-variables
@@ -17,7 +17,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (evil-surround evil-org evil-numbers evil-matchit evil-mark-replace evil-nerd-commenter evil-leader which-key use-package try powerline moe-theme evil-args counsel color-theme auto-complete))))
+    (highlight-indent-guides evil-surround evil-org evil-numbers evil-matchit evil-mark-replace evil-nerd-commenter evil-leader which-key use-package try powerline moe-theme evil-args counsel color-theme auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

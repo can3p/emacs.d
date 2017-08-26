7dcc88e06fe6ad07df8b336e6eee20796d25347c
Dmitry Petrov <dpetroff@gmail.com>
2017-08-26 23:30:56 +0200

    enable ctrlp like fuzzy search for ivy and add more information into swith buffer

--- init.el
+++ init.el
@@ -15,9 +15,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
  '(package-selected-packages
    (quote
-    (powerline-evil evil-lion move-text highlight-parentheses markdown-mode highlight-indent-guides evil-surround evil-org evil-numbers evil-matchit evil-mark-replace evil-nerd-commenter evil-leader which-key use-package try powerline moe-theme evil-args counsel color-theme auto-complete))))
+    (ivy-rich solarized-theme powerline-evil evil-lion move-text highlight-parentheses markdown-mode highlight-indent-guides evil-surround evil-org evil-numbers evil-matchit evil-mark-replace evil-nerd-commenter evil-leader which-key use-package try powerline moe-theme evil-args counsel color-theme auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

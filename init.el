40d77be38897c4b4cb1d143111acde158536d822
Dmitry Petrov <dpetroff@gmail.com>
2017-10-20 23:25:39 +0200

    magit and web-mode

--- init.el
+++ init.el
@@ -17,11 +17,11 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+    ("b9a06c75084a7744b8a38cb48bc987de10d68f0317697ccbd894b2d0aca06d2b" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(initial-frame-alist (quote ((fullscreen . maximized))))
  '(package-selected-packages
    (quote
-    (persp-projectile projectile perspective edit-indirect slime ivy-rich solarized-theme powerline-evil evil-lion move-text highlight-parentheses markdown-mode highlight-indent-guides evil-surround evil-org evil-numbers evil-matchit evil-mark-replace evil-nerd-commenter evil-leader which-key use-package try powerline moe-theme evil-args counsel color-theme auto-complete))))
+    (web-mode magit persp-projectile projectile perspective edit-indirect slime ivy-rich solarized-theme powerline-evil evil-lion move-text highlight-parentheses markdown-mode highlight-indent-guides evil-surround evil-org evil-numbers evil-matchit evil-mark-replace evil-nerd-commenter evil-leader which-key use-package try powerline moe-theme evil-args counsel color-theme auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
